7d275db9f6020291f39f47e07afca2c2e4ce50e0
TessFerrandez <tferrand@microsoft.com>
2021-12-05 00:48:31 +0100

    2021 day 4

--- y2021/day4/Program.cs
+++ y2021/day4/Program.cs
@@ -1,92 +1,81 @@
-﻿string[] Numbers() => File.ReadAllLines("c:\\AOC\\03.txt").ToArray();
+﻿string[] Lines() => File.ReadAllLines("c:\\AOC\\04.txt").ToArray();
 
-int GammaRate(string[] numbers){
-    string result = "";
-
-    for (int i = 0; i < numbers[0].Length; i++){
-        int zeros = 0;
-        int ones = 0;
-        foreach(var number in numbers){
-            if (number[i] == '0'){
-                zeros++;
-            } else {
-                ones++;
-            }
-        }
-        if(zeros > ones){
-            result += '0';
-        } else {
-            result += '1';
-        }
+Dictionary<int, int> CallOrder(string calls){
+    var dict = new Dictionary<int, int>();
+    int[] numbers = calls.Split(',').Select(int.Parse).ToArray();
+    for(int i = 0; i < numbers.Length; i++){
+        dict.Add(numbers[i], i);
     }
-    return Convert.ToInt32(result, 2);
+    return dict;
 }
 
-int EpsilonRate(string[] numbers){
-    string result = "";
+List<List<List<int>>> Boards(string[] Lines){
+    var boards = new List<List<List<int>>>();
+    int row = 0;
+    List<List<int>> board = null;
 
-    for (int i = 0; i < numbers[0].Length; i++){
-        int zeros = 0;
-        int ones = 0;
-        foreach(var number in numbers){
-            if (number[i] == '0'){
-                zeros++;
-            } else {
-                ones++;
-            }
+    foreach(string line in Lines){
+        if(row == 0){
+            row++;
+            continue;
         }
-        if(zeros > ones){
-            result += '1';
-        } else {
-            result += '0';
+        if(line == ""){
+            if(board != null){
+                boards.Add(board);
+            }
+            board = new List<List<int>>();
+        }
+        else{
+            board.Add(line.Split(' ').Where(x => x != "").Select(int.Parse).ToList());
         }
     }
-    return Convert.ToInt32(result, 2);
+    boards.Add(board);
+    return boards;
 }
 
-int OxygenRate(string[] numbers){
-    int numLenght = numbers[0].Length;
+int[] WinningMoves(List<List<List<int>>> boards, Dictionary<int, int> callOrder){
+    var winningMoves = new int[boards.Count];
 
-    for (int i = 0; i < numLenght; i++){
-        int zeros = 0;
-        int ones = 0;
-        foreach(var number in numbers){
-            if (number[i] == '0'){
-                zeros++;
-            } else {
-                ones++;
-            }
+    int boardIndex = 0;
+    foreach(var board in boards){
+        var leastMoves = callOrder.Count;
+
+        // rows
+        foreach(var row in board){
+            var rowWinningMove = row.Select(x => callOrder[x]).Max();
+            leastMoves = Math.Min(leastMoves, rowWinningMove);
         }
-        char bit = ones >= zeros? '1' : '0';
-        numbers = numbers.Where(x => x[i] == bit).ToArray();
-        if (numbers.Length == 1){
-            return Convert.ToInt32(numbers[0], 2);
+
+        // columns
+        for(int col = 0; col < 5; col++){
+            var colWinningMove = board.Select(row => row[col]).ToList().Select(x => callOrder[x]).Max();
+            leastMoves = Math.Min(leastMoves, colWinningMove);
         }
+        winningMoves[boardIndex] = leastMoves;
+        boardIndex++;
     }
-    return 0;
+    return winningMoves;
 }
 
-int CO2Rate(string[] numbers){
-    int numLenght = numbers[0].Length;
-
-    for (int i = 0; i < numLenght; i++){
-        int zeros = 0;
-        int ones = 0;
-        foreach(var number in numbers){
-            if (number[i] == '0'){
-                zeros++;
-            } else {
-                ones++;
-            }
-        }
-        char bit = ones >= zeros ? '0' : '1';
-        numbers = numbers.Where(x => x[i] == bit).ToArray();
-        if (numbers.Length == 1){
-            return Convert.ToInt32(numbers[0], 2);
-        }
+int CalculateBoard(List<List<int>> board, Dictionary<int, int> callOrder, int move){
+    int sumOfNotCalled = 0;
+    foreach(var row in board){
+        sumOfNotCalled += row.Where(x => callOrder[x] > move).Sum();
     }
-    return 0;
+
+    int lastMove = callOrder.Where(x => x.Value == move).Select(x => x.Key).First();
+
+    return sumOfNotCalled * lastMove;
 }
 
-Console.WriteLine(GammaRate(Numbers()) * EpsilonRate(Numbers()));
-Console.WriteLine(OxygenRate(Numbers()) * CO2Rate(Numbers()));
+var callOrder = CallOrder(Lines()[0]);
+var boards = Boards(Lines());
+var winningMoves = WinningMoves(boards, callOrder);
+
+int winningBoardNumber = winningMoves.ToList().IndexOf(winningMoves.Min());
+List<List<int>> winningBoard = boards[winningBoardNumber];
+Console.WriteLine(CalculateBoard(winningBoard, callOrder, winningMoves[winningBoardNumber]));
+
+int loosingBoardNumber = winningMoves.ToList().IndexOf(winningMoves.Max());
+List<List<int>> loosingBoard = boards[loosingBoardNumber];
+Console.WriteLine(CalculateBoard(loosingBoard, callOrder, winningMoves[loosingBoardNumber]));
